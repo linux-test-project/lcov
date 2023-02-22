cdde0d68ec6ac00fb326541e5a03d152fd629b29
Henry Cox <Henry.Cox@mediatek.com>
2023-02-22 10:12:33 -0500

    If set, use $ENV{SOURCE_DATE_EPOCH} to compute file age (for date bin) - otherwise, use 'now' (when report is generated).
    Signed-off-by:  Henry Cox <henry.cox@mediatek.com>

--- tests/gendiffcov/simple/annotate.sh
+++ tests/gendiffcov/simple/annotate.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
+#!/bin/env perl
 
-FILE=$1
-ANNOTATED=${FILE}.annotated
-if [ -f $ANNOTATED ]; then
-        cat ${1}.annotated
-else
-        sed -e 's/^/NONE|NONE|1900-01-01T00:00:01-05:05|/' <$FILE
-fi
+use strict;
+use warnings;
+use DateTime;
+use POSIX qw(strftime);
+
+my $file      = $ARGV[0];
+my $annotated = $file . ".annotated";
+my $now       = DateTime->now();
+
+if (open(HANDLE, '<', $annotated)) {
+    while (my $line = <HANDLE>) {
+        chomp $line;
+        $line =~ s/\r//g;    # remove CR from line-end
+        my ($commit, $who, $days, $text) = split(/\|/, $line, 4);
+        my $duration = DateTime::Duration->new(days => $days);
+        my $date     = $now - $duration;
+
+        printf("%s|%s|%s|%s\n", $commit, $who, $date, $text);
+    }
+    close(HANDLE) or die("unable to close $annotated: $!");
+} else {
+    die("unable to open $annotated: $!");
+}
