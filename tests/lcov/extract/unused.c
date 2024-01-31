f9b15615506ce28a7f7acd3e07cc1e46f75a3e89
Henry Cox <henry.cox@mediatek.com>
2024-01-31 09:29:04 -0500

    'geninfo --capture --all' feature:   If --all flag is specified, then capture from both runtime (.gcda) and   bare compile-time (.gcno) files - i.e., include unused .gcno files   which have no corresponding runtime data.
    Signed-off-by:  Henry Cox <henry.cox@mediatek.com>

---
+++ tests/lcov/extract/unused.c
@@ -0,0 +1,9 @@
+/* this code is not linked into the exe/not called.
+   Testing 'initial' capture and combined capture */
+#include <stdio.h>
+
+void f(int y)
+{
+  if (y)
+    printf("y == %d\n", y);
+}
