6b8acb3325556ec5a82505cce6634cefa12c39e8
Henry Cox <68852529+henry2cox@users.noreply.github.com>
2025-09-01 10:06:02 -0400

    Add 'unreachable region' feature. (#433)
    This is similiar to 'excluded regions' - except that lcov will generate an 'unreachable' message if any coverpoints in the region are hit.
    
    Signed-off-by: Henry Cox <henry.cox@mediatek.com>

--- tests/lcov/extract/extract.cpp
+++ tests/lcov/extract/extract.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, const char *argv[])
+int main(int argc, const char *argv[]) // TEST_UNREACH_FUNCTION
 {
   bool b = false;
   if (strcmp(argv[1], "1") == 0)
@@ -18,17 +18,19 @@ int main(int argc, const char *argv[])
 
   // TEST_OVERLAP_START
   // TEST_OVERLAP_START
+  // TEST_UNREACHABLE_START
   std::string str("asdads");
+  // TEST_UNREACHABLE_END
   str = "cd";
   // TEST_OVERLAP_END
 
   //TEST_DANGLING_START
   //TEST_UNMATCHED_END
 
-  std::cout << str << std::endl;
+  std::cout << str << std::endl; // TEST_UNREACHABLE_LINE
 
-  // LCOV_EXCL_START
+  // LCOV_EXCL_START_1
   std::cout << "adding some code to ignore" << std::endl;
-  // LCOV_EXCL_STOP
+  // LCOV_EXCL_STOP_1
   return 0;
 }
